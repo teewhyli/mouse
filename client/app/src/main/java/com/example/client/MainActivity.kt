f57934b252e3c1036f4d4d679cc7924efe0105fd
Tianyang Li <teewhyli.tl@gmail.com>
2021-08-09 20:38:50 +1200

    general layout for client side mouse interface

--- client/app/src/main/java/com/example/client/MainActivity.kt
+++ client/app/src/main/java/com/example/client/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.example.client
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.*
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val layout: View = findViewById(R.id.touch_board)
+//        val controller = layout.windowInsetsController
+//
+//        controller?.show(WindowInsets.Type.ime())
+
+        val editText: EditText = findViewById(R.id.edit_board_input)
+        editText.requestFocus()
+//        val imm = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+//        imm.showSoftInput(editText, InputMethodManager.SHOW_IMPLICIT)
+
+        layout.setOnTouchListener { v, event ->
+            println(event)
+            return@setOnTouchListener true
+        }
     }
 }
