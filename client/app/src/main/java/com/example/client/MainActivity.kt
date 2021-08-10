8551f223f51a5939a6aa62f661ee059625e4cee9
Tianyang Li <teewhyli.tl@gmail.com>
2021-08-11 00:44:28 +1200

    add listeners for keyboard onKeyUp and toggling of hide and show of keyboard

--- client/app/src/main/java/com/example/client/MainActivity.kt
+++ client/app/src/main/java/com/example/client/MainActivity.kt
@@ -1,30 +1,61 @@
 package com.example.client
 
 import android.os.Bundle
+import android.view.KeyEvent
+import android.view.KeyEvent.*
 import android.view.View
-import android.widget.EditText
+import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.*
+import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton
+import java.util.*
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), Callback {
+
+    private lateinit var layout: View;
+    private lateinit var extFloatingButton: ExtendedFloatingActionButton;
+    private lateinit var imm: InputMethodManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val layout: View = findViewById(R.id.touch_board)
-//        val controller = layout.windowInsetsController
-//
-//        controller?.show(WindowInsets.Type.ime())
+        layout = findViewById(R.id.touch_board)
+        extFloatingButton = findViewById(R.id.extended_fab)
+        imm = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
 
-        val editText: EditText = findViewById(R.id.edit_board_input)
-        editText.requestFocus()
-//        val imm = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
-//        imm.showSoftInput(editText, InputMethodManager.SHOW_IMPLICIT)
+        extFloatingButton.setOnClickListener { showSoftKeyboard() }
 
-        layout.setOnTouchListener { v, event ->
+        layout.setOnTouchListener { _, event ->
             println(event)
             return@setOnTouchListener true
+        }
+    }
+
+    private fun showSoftKeyboard() {
+        imm.toggleSoftInput(InputMethodManager.SHOW_FORCED,0)
+    }
+
+    private fun hideSoftKeyboard() {
+        imm.hideSoftInputFromWindow(this.currentFocus?.windowToken, 0)
+    }
+
+    override fun onKeyUp(keyCode: Int, event: KeyEvent): Boolean {
+        if (event.isShiftPressed){
+            println("shifted")
+        }
+        return when (keyCode) {
+            KEYCODE_ENTER -> {
+                hideSoftKeyboard()
+                true
+            }
+            KEYCODE_SHIFT_LEFT -> { true }
+            else -> {
+                println(event)
+                println(keyCode)
+                super.onKeyUp(keyCode, event)
+            }
         }
     }
 }
